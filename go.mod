65b72b9ecc61c2c751f63d8423146f31ab5d3d9e
Luis Albarracín <luisalbarracin92@hotmail.com>
2026-02-28 19:49:14 +0100

    Tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mandarinomczumo/pokedexcli
+
+go 1.25.0
---
+++ repl_test.go
@@ -0,0 +1,38 @@
+package pokedexcli
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		// add more cases here
+	}
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice against the expected slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("length doesn't match!")
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+			if word != expectedWord {
+				t.Errorf("word %s doesn't match expected word %s", word, expectedWord)
+			}
+		}
+	}
+}
