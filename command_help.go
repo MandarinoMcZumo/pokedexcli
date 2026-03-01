496dd4de1b6a1d124f4eab236005caef41d264d3
Luis Albarracín <luisalbarracin92@hotmail.com>
2026-03-01 09:57:29 +0100

    Refactor to avoid lambdas

---
+++ command_help.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func commandHelp() error {
+	commands := getCommands()
+	fmt.Println(`Welcome to the Pokedex!
+Usage:`)
+	for _, cmd := range commands {
+		fmt.Println(cmd.name+": ", cmd.description)
+	}
+	return nil
+}
--- repl.go
+++ repl.go
@@ -15,42 +15,20 @@ func cleanInput(text string) []string {
 	return cleanText
 }
 
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-// func commandHelp() {
-// 	exitCmd := cliMedatada{name: "exit", description: "Exit the Pokedex"}
-// 	helpCmd := cliMedatada{name: "help", description: "Displays a help message"}
-// 	commands := []cliMedatada{exitCmd, helpCmd}
-// 	fmt.Println(`Welcome to the Pokedex!
-// 			Usage:`)
-// 	for _, cmd := range commands {
-// 		fmt.Println(cmd.name+": ", cmd.description)
-// 	}
-// }
-
 type cliCommand struct {
 	name        string
 	description string
 	callback    func() error
 }
 
-func main() {
-	commands := map[string]cliCommand{}
+func getCommands() map[string]cliCommand {
 	exitCmd := cliCommand{name: "exit", description: "Exit the Pokedex", callback: commandExit}
-	helpCmd := cliCommand{name: "help", description: "Displays a help message", callback: func() error {
-		fmt.Println(`Welcome to the Pokedex!
-Usage:`)
-		for _, cmd := range commands {
-			fmt.Println(cmd.name+": ", cmd.description)
-		}
-		return nil
-	}}
-	commands["exit"] = exitCmd
-	commands["help"] = helpCmd
+	helpCmd := cliCommand{name: "help", description: "Displays a help message", callback: commandHelp}
+	return map[string]cliCommand{"exit": exitCmd, "help": helpCmd}
+}
+
+func main() {
+	commands := getCommands()
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
@@ -61,10 +39,12 @@ Usage:`)
 		command, ok := commands[input]
 		if !ok {
 			fmt.Println("Unknown command")
+			continue
 		}
 		err := command.callback()
 		if err != nil {
 			fmt.Println("error", err)
+			continue
 		}
 	}
 }
